69e1145a4a3e5c152bc11756b67e643993f5a66f
TIMcre <nhom@web.de>
2022-11-03 14:34:45 +0100

    modified formula_typer.py

--- formula_typer.py
+++ formula_typer.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
 website = str(input("website url"))
 # inital setup
-driver = webdriver.Firefox(executable_path="geckodriver.exe")
+s = Service("geckodriver.exe")
+driver = webdriver.Firefox(service=s)
 try:
     driver.get(website)
 except:
-    print("invalid url")
+    print("invalid url trying default one...")
+    try:
+        driver.get("https://anton.app/de/")
+    except:
+        print("conection error")
+
