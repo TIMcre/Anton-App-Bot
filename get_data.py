458458e42fa1c156e5f746506de87b485547d6e0
TIMcre <nhom@web.de>
2022-11-04 14:07:14 +0100

    modified get_data.py

--- get_data.py
+++ get_data.py
@@ -1,23 +1,35 @@
 import requests
 import json
-from nested_lookup import nested_lookup
-import regex as re
 
-url = "https://content.anton.app/files/?fileId=level%2Fc-mat-9%2Ftopic-01-wurzeln-und-potenzen%2Fblock-05-potenzgesetze%2Flevel-02&etag=d76b-4804"
+from nested_lookup import nested_lookup
+import re
+
+
+url = "https://content.anton.app/files/?fileId=level%2Fc-mat-9%2Ftopic-01-wurzeln-und-potenzen%2Fblock-05-potenzgesetze%2Flevel-test&etag=5594-3691"
+
 
 def get_json(url):
+
     data = requests.request("GET", url).json()
     return data
 
+
 def sort_json(data):
+
     question_list = []
+
     question_list = nested_lookup("b", data)
-    re.sub("/ ^\< \>$/", "", question_list)
+    question_list = re.sub("<.*?>|/d|\|+", "", str(question_list)).strip("][").split(', ')
     return question_list
+    
+
 
 def print_data(data):
+
     for item in data:
+
         print(item,end="\n")
+
 
 data = get_json(url)
 data = sort_json(data)
