df23eb9c6f1c57cfa08838c33438b1f299a61de3
TIMcre <nhom@web.de>
2023-01-29 14:34:09 +0100

    update formula_typer.py and main.py

--- formula_typer.py
+++ formula_typer.py
@@ -1,26 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from get_data import Console
-import time
-
-cn = Console()
-website = cn.inp("Input", "website url")
-
-# inital setup
-s = Service("chromedriver.exe")
-driver = webdriver.Chrome(service=s)
-
-try:
-    driver.get(website)
-except:
-    cn.write("Error", "invalid url trying default one...")
-    try:
-        driver.get("https://anton.app/de/")
-    except:
-        cn.write("Error", "conection error")
+from main import *
 
 
-while True:
+while not login:
     pass
+while True:
+    try:
+        input_box_1 = driver.find_element(By.CSS_SELECTOR, "")
+        time.sleep(0.5)
+        input_box_1.send_keys("test")
+    except:
+        pass
+
+
+    
    
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from get_data import Console
+import time
+
+username = "as2c-hcnm"
+
 def login(username):
     try:
         nbox1 = driver.find_element(By.CSS_SELECTOR ,"body > div.pageWrapper.onDomAppend > div.scrollDiv.pageScrollWrapper.allowBackSwipe.onStatusBarTap > div.scrollDivContainer.onWindowResize.onDomRemove.onDomAppend > div > div > div:nth-child(2) > div.page > div > div:nth-child(1) > div:nth-child(1) > div > div.onSubscribeLogsDone.onNewLogEvents_setAvatar.onNewLogEvents_setName > div > div:nth-child(1)")
@@ -19,3 +27,27 @@ def login(username):
 
     except: 
         return False
+
+def initial_setup():
+    # inital setup
+    cn = Console()
+    website = cn.inp("Input", "website url")
+
+    s = Service("chromedriver.exe")
+    driver = webdriver.Chrome(service=s)
+
+    try:
+        driver.get(website)
+    except:
+        cn.write("Error", "invalid url trying default one...")
+    try:
+        driver.get("https://anton.app/")
+    except:
+        cn.write("Error", "conection error")
+
+
+if __name__ == '__main__':
+    initial_setup()
+    time.sleep(2)
+    login(username)
+
